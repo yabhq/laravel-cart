bff1edda44ac362eabcb71b517bbd1652ede36c2
Jim <jimhlad@gmail.com>
2020-12-17 17:28:58 -0500

    Tweak test case for custom fields

--- tests/Feature/Api/CheckoutItemTest.php
+++ tests/Feature/Api/CheckoutItemTest.php
@@ -15,7 +15,6 @@ class CheckoutItemTest extends TestCase
     /** @test */
     public function a_new_cart_item_can_be_created_via_the_api()
     {
-        $this->withoutExceptionHandling();
         $product = factory(Product::class)->create([
             'price' => 14.95,
         ]);
@@ -26,6 +25,7 @@ class CheckoutItemTest extends TestCase
             'purchaseable_id' => $product->id,
             'purchaseable_type' => $product->getMorphClass(),
             'qty' => 1,
+            'options' => [ 'color' => 'green' ],
         ]);
     
         $response->assertSuccessful();
@@ -37,6 +37,7 @@ class CheckoutItemTest extends TestCase
             'qty' => 1,
             'unit_price' => 1495,
             'price' => 1495,
+            'custom_fields' => json_encode([ 'options' => [ 'color' => 'green' ]], )
         ]);
     }
 
